5b446fa51d25d795fdb07ea62175fd8883775a6b
LUIS GUSTAVO DE JESUS HUNECHER <lgustavo@websupply.com.br>
2024-05-21 21:24:42 -0300

    Finalização das Models Natureza, Plano de Contas, Tipo de Operação e Unidades vs Usuarios

---
+++ WebsupplyHHemo.Interface/Model/NaturezaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsupplyHHemo.Interface.Model
+{
+    public class NaturezaModel
+    {
+        public string CodNatureza { get; set;  }
+        public string Descricao { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ WebsupplyHHemo.Interface/Model/PlanoContaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsupplyHHemo.Interface.Model
+{
+    public class PlanoContaModel
+    {
+        public string ContaContabil { get; set; }
+        public string Descricao { get; set; }
+        public string Status { get; set; }
+    }
+}
