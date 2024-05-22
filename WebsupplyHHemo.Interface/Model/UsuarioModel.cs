ce9c2d67e20765184119ead64b81915ddd3fe8b4
LUIS GUSTAVO DE JESUS HUNECHER <lgustavo@websupply.com.br>
2024-05-21 21:23:53 -0300

    Finalização da Interface de Usuários

---
+++ WebsupplyHHemo.Interface/Model/UsuarioModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsupplyHHemo.Interface.Model
+{
+    public class UsuarioModel
+    {
+        public string CodUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Comprador { get; set; }
+        public string Solicitante { get; set; }
+        public string Status { get; set; }
+    }
+}
