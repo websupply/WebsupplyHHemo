6640a42f556f9697ea87b7a1785a16577e085649
Luis Gustavo de Jesus Hunzecher <lgustavo@websupply.com.br>
2024-07-15 20:23:26 -0300

    Finalização da Estrutura de Recebimento Fiscal

---
+++ WebsupplyHHemo.API/ADO/RecebimentoADO.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using WebsupplyHHemo.API.Dto;
+
+namespace WebsupplyHHemo.API.ADO
+{
+    public class RecebimentoADO
+    {
+        public string strMensagem = string.Empty;
+
+        public bool ATUALIZA_RECEBIMENTO_FISCAL(string Connection, RecebimentoRequestDto objRequest)
+        {
+            // Seta os Parametros Iniciais de Retorno
+            bool retorno = false;
+            strMensagem = $"Erro durante a realização do Recebimento Fiscal do número [{objRequest.NumDoc}] referente ao pedido [{objRequest.NumPedido}]";
+
+            ConexaoSQLServer Conn = new ConexaoSQLServer(Connection);
+
+            string NomeProcedure = "SP_HHemo_WS_Produtos_Precos_CCONTABIL_CLI_UPD";
+
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            //parametros.Add(new SqlParameter("@cCodProd", objRequest.CodWebsupply));
+            //parametros.Add(new SqlParameter("@cCodProdutoCompleto", objRequest.CodProtheus));
+            //parametros.Add(new SqlParameter("@cCCONTABIL_CLI", objRequest.ContaContabil));
+
+            using (var reader = Conn.ExecutaComParametros(NomeProcedure, parametros))
+            {
+                while (reader.HasRows)
+                {
+                    strMensagem = $"Recebimento Fiscal do número [{objRequest.NumDoc}] referente ao pedido [{objRequest.NumPedido}] realizado com sucesso";
+                    retorno = true;
+                }
+            }
+
+            Conn.Dispose();
+
+            return retorno;
+        }
+    }
+}
--- WebsupplyHHemo.API/Controllers/RecebimentoController.cs
+++ WebsupplyHHemo.API/Controllers/RecebimentoController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WebsupplyHHemo.API.ADO;
 using WebsupplyHHemo.API.Dto;
+using WebsupplyHHemo.API.Helpers;
+using WebsupplyHHemo.API.Models;
 
 namespace WebsupplyHHemo.API.Controllers
 {
@@ -16,9 +20,65 @@ namespace WebsupplyHHemo.API.Controllers
 
         [HttpPost]
         [Route("envia-recebimento")]
-        public Object EnviaRecebimento(RecebimentoRequestDto objRequest)
+        public ObjectResult ComplementoItem(RecebimentoRequestDto objRequest)
         {
-            return new Object();
+            // Pega a Claims
+            ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
+            UserModel objUser = HelperClaims.CarregarUsuario(identity);
+
+            // Instancia o ADO do Log
+            LogDeOperacaoModel objLog = new LogDeOperacaoModel();
+
+            // Gera o Log de Operação
+            objLog.nCod_Operacao = _configuration.GetValue<int>("Parametros:LogDeOperacao:Operacoes_Tipos-Codigo_Recebimento_Fiscal_Integracao");
+            objLog.cDetalhe = $"Recebimento Fiscal do número [{objRequest.NumDoc}] referente ao pedido [{objRequest.NumPedido}] realizado com sucesso. (Apenas mensagem de teste)"; ;
+            objLog.cIP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            return new ObjectResult(new
+            {
+                Mensagem = "Requisição Realizada com sucesso"
+            })
+            { StatusCode = 200 };
+
+            // Instancia o ADO do Complemento Contabil do Item
+            ComplementoContabilItemADO objADO = new ComplementoContabilItemADO();
+
+            if (!objADO.ATUALIZA_DADOS_CONTABEIS_ITEM(_configuration.GetValue<string>("ConnectionStrings:DefaultConnection"), objRequest))
+            {
+                // Gera o Log de Operação
+                objLog.nCod_Operacao = _configuration.GetValue<int>("Parametros:LogDeOperacao:Operacoes_Tipos-Codigo_Conta_Contabil_Integracao");
+                objLog.cDetalhe = objADO.strMensagem;
+                objLog.cIP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                LogsADO.GERA_LOGDEOPERACAO(
+                    _configuration.GetValue<string>("ConnectionStrings:DefaultConnection"),
+                    objUser,
+                    objLog
+                );
+
+                return new ObjectResult(new
+                {
+                    Mensagem = objADO.strMensagem
+                })
+                { StatusCode = 500 };
+            }
+
+            // Gera o Log de Operação
+            objLog.nCod_Operacao = _configuration.GetValue<int>("Parametros:LogDeOperacao:Operacoes_Tipos-Codigo_Conta_Contabil_Integracao");
+            objLog.cDetalhe = objADO.strMensagem;
+            objLog.cIP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            LogsADO.GERA_LOGDEOPERACAO(
+                _configuration.GetValue<string>("ConnectionStrings:DefaultConnection"),
+                objUser,
+                objLog
+            );
+
+            return new ObjectResult(new
+            {
+                Mensagem = objADO.strMensagem
+            })
+            { StatusCode = 200 };
         }
     }
 }
