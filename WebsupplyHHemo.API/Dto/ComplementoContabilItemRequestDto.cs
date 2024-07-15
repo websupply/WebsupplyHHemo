53e465404f335378354e69fdfbcb5f17bc5bbaab
Luis Gustavo de Jesus Hunzecher <lgustavo@websupply.com.br>
2024-07-15 16:28:43 -0300

    Inclusão da Obrigatoriedade dos campos do complemento contabil do item

--- WebsupplyHHemo.API/Dto/ComplementoContabilItemRequestDto.cs
+++ WebsupplyHHemo.API/Dto/ComplementoContabilItemRequestDto.cs
@@ -1,9 +1,16 @@
-﻿namespace WebsupplyHHemo.API.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsupplyHHemo.API.Dto
 {
     public class ComplementoContabilItemRequestDto
     {
+        [Required(ErrorMessage = "CodProtheus é obrigatório")]
         public string CodProtheus { get; set; }
+
+        [Required(ErrorMessage = "CodWebsupply é obrigatório")]
         public string CodWebsupply { get; set; }
+
+        [Required(ErrorMessage = "Conta Contábil é obrigatória")]
         public string ContaContabil { get; set; }
     }
 }
