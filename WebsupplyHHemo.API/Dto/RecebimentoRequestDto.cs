3c900f04bd4bbb047cd88de8591536940fda43ae
Luis Gustavo de Jesus Hunzecher <lgustavo@websupply.com.br>
2024-07-16 12:03:33 -0300

    Atualização do Dto de Recebimento Fiscal adicionando obrigatoriedade nos campos.

--- WebsupplyHHemo.API/Dto/RecebimentoRequestDto.cs
+++ WebsupplyHHemo.API/Dto/RecebimentoRequestDto.cs
@@ -1,12 +1,25 @@
-﻿namespace WebsupplyHHemo.API.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsupplyHHemo.API.Dto
 {
     public class RecebimentoRequestDto
     {
+        [Required(ErrorMessage = "NumPedido é obrigatório")]
         public string NumPedido { get; set; }
+
+        [Required(ErrorMessage = "NumDoc é obrigatório")]
         public string NumDoc { get; set; }
+
+        [Required(ErrorMessage = "Serie é obrigatório")]
         public string Serie { get; set; }
+
+        [Required(ErrorMessage = "CodFornecedor é obrigatório")]
         public string CodFornecedor { get; set; }
+
+        [Required(ErrorMessage = "CnpjFornecedor é obrigatório")]
         public string CnpjFornecedor { get; set; }
+
+        [Required(ErrorMessage = "Status é obrigatório")]
         public string Status { get; set; }
     }
 }
